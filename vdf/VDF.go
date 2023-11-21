f082f656d1aa5072699af559ac1b1db5678f4bdf
Sir Gallo <nickgallo97@gmail.com>
2023-11-21 01:10:02 -0500

    fix formatting

--- vdf/VDF.go
+++ vdf/VDF.go
@@ -7,13 +7,13 @@ import "math/big"
 func VDF(version uint64, prevSuccessfulWrites uint64) *big.Int {
 	vNextBig := big.NewInt(int64(version))
 
-  p := new(big.Int)
+	p := new(big.Int)
 	p.SetString(P, 16)
 
-  Ns := int64(N_BASE) * int64(math.Sqrt(float64(prevSuccessfulWrites)))
-  NsBig := big.NewInt(Ns)
+	Ns := int64(N_BASE) * int64(math.Sqrt(float64(prevSuccessfulWrites)))
+	NsBig := big.NewInt(Ns)
 
-  exponent := new(big.Int).Exp(big.NewInt(2), NsBig, nil)
-  exponentiatedVNext := new(big.Int).Exp(vNextBig, exponent, nil)
+	exponent := new(big.Int).Exp(big.NewInt(2), NsBig, nil)
+	exponentiatedVNext := new(big.Int).Exp(vNextBig, exponent, nil)
 	return new(big.Int).Exp(big.NewInt(G), exponentiatedVNext, p)
 }
